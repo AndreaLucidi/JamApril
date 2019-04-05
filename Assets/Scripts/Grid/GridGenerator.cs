3f4bb6a6b008b97e755af7a2dbe15c8413d52a32
Sugni <giaco.salvadori@gmail.com>
2019-04-05 17:44:28 +0200

    [TECH] Added tile informations

--- Assets/Scripts/Grid/GridGenerator.cs
+++ Assets/Scripts/Grid/GridGenerator.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
     #region Properties
+    //PUBLIC
     [Header("Grid properties")]
     public float cellSize = 1.0f;
     public int rows;
     public int cols;
     [Header("Object for tiling")]
     public GameObject tile;
+    //PRIVATE
+    private Vector3[,] points;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        points = new Vector3[rows, cols];
         CreateGrid();
     }
 
@@ -40,6 +44,7 @@ public class GridGenerator : MonoBehaviour
                 Vector3 pos = transform.position + (new Vector3(x, 0.0f, y) * cellSize);
                 GameObject tileObj = Instantiate(tile, pos, Quaternion.identity, transform);
                 tileObj.name = "Tile_" + (x+1).ToString() + "_" + (y+1).ToString();
+                tileObj.GetComponent<Tile>().SetCoordinates(x, y);
             }
         }
     }
---
+++ Assets/Scripts/Grid/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    #region Properties
+    // PUBLIC
+    public int x;
+    public int y;
+    // PRIVATE
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCoordinates(int x_, int y_)
+    {
+        x = x_;
+        y = y_;
+    }
+}
